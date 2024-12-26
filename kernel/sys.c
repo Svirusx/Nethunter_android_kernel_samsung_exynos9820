4fefc8c9437fa350f816252c8350d650517fc465
chiteroman <querveros999@gmail.com>
2024-12-26 22:48:06 +0100

    SUSfs patches

--- kernel/sys.c
+++ kernel/sys.c
@@ -1384,19 +1384,39 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+<<<<<<< HEAD
+=======
+
+static uint64_t netbpfload_pid = 0;
+
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern int susfs_spoof_uname(struct new_utsname* tmp);
+#endif
+
+
+>>>>>>> 2db918893a7b (SUSfs patches)
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	if (likely(!susfs_spoof_uname(&tmp)))
+		goto bypass_orig_flow;
+#endif
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAMEAdd commentMore actions
+bypass_orig_flow:
+#endif
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "5.4.186");
+
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
+
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
